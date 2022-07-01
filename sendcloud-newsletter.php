d4ad88f1518c21656f29ff962baa4259b15df2b8
jabber <2898117012@qq.com>
2022-07-02 01:02:14 +0800

    init

--- sendcloud-newsletter.php
+++ sendcloud-newsletter.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Plugin Name: Sendcloud Newsletter
+ * Plugin URI: https://www.shopeo.cn
+ * Description: The integrated <code>sendcloud.net</code> newsletter management system provides the latest news and mailing channels.
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://www.shopeo.cn
+ * License: GPL2+
+ * Text Domain: sendcloud-newsletter
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
