c1bb9f2e7aaa09afda8a5bc74fda5473b5ceb967
jabber <2898117012@qq.com>
2022-07-04 02:00:10 +0800

    init

--- sendcloud-newsletter.php
+++ sendcloud-newsletter.php
@@ -13,6 +13,23 @@
  * Requires PHP: 5.6
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Define SENDCLOUD_NEWSLETTER_PLUGIN_FILE.
+if ( ! defined( 'SENDCLOUD_NEWSLETTER_PLUGIN_FILE' ) ) {
+	define( 'SENDCLOUD_NEWSLETTER_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'SENDCLOUD_NEWSLETTER_PLUGIN_BASE' ) ) {
+	define( 'SENDCLOUD_NEWSLETTER_PLUGIN_BASE', plugin_basename( SENDCLOUD_NEWSLETTER_PLUGIN_FILE ) );
+}
+
+if ( ! defined( 'SENDCLOUD_NEWSLETTER_PATH' ) ) {
+	define( 'SENDCLOUD_NEWSLETTER_PATH', plugin_dir_path( SENDCLOUD_NEWSLETTER_PLUGIN_FILE ) );
+}
+
 if ( ! function_exists( 'sendcloud_newsletter_activate' ) ) {
 	function sendcloud_newsletter_activate() {
 
@@ -28,3 +45,11 @@ if ( ! function_exists( 'sendcloud_newsletter_deactivate' ) ) {
 }
 
 register_deactivation_hook( __FILE__, 'sendcloud_newsletter_deactivate' );
+
+if ( ! function_exists( 'sendcloud_newsletter_load_textdomain' ) ) {
+	function sendcloud_newsletter_load_textdomain() {
+		load_plugin_textdomain( 'sendcloud-newsletter', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'sendcloud_newsletter_load_textdomain' );
