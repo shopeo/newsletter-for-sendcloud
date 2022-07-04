c9c955fac855ccd1c7d46ff4ebdb83c04dfd5787
jabber <2898117012@qq.com>
2022-07-05 04:17:19 +0800

    init

--- includes/SubscribeWidget.class.php
+++ includes/SubscribeWidget.class.php
@@ -63,7 +63,6 @@ class SubscribeWidget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance          = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-		$instance['text']  = ( ! empty( $new_instance['text'] ) ) ? $new_instance['text'] : '';
 
 		return $instance;
 	}
